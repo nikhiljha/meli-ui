67545f5e63c698f501448d4ee96284038d18e55e
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-12-07 11:34:31 +0100

    fix(site settings): missing default value for mainBranch

--- src/components/sites/settings/SelectMainBranch.tsx
+++ src/components/sites/settings/SelectMainBranch.tsx
@@ -56,6 +56,7 @@ export function SelectMainBranch({ siteId }: {
         rules={{
           required,
         }}
+        defaultValue={undefined}
         render={({ value, onChange }) => (
           <CustomSelect
             options={branches?.map(b => ({
